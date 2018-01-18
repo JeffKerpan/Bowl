5972139120480ef581da7b14fcbf8e2d539c7fc7
Jeff Kerpan <jkerpan@gmail.com>
2018-01-18 13:16:43 -0700

    Final Score with multiple strikes test passing

--- Test/getFinalScore.spec.js
+++ Test/getFinalScore.spec.js
@@ -4,20 +4,32 @@ import { expect } from 'chai';
 
 describe('Final Score', () => {
 
-  it('Should get a final score from a full game', () => {
+  it('Should get final score from full game', () => {
     let rolls = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
 
     expect(getFinalScore(rolls)).to.deep.equal(30);
   })
 
-  it('Should get a final score from a gutter ball full game', () => {
+  it('Should get final score from gutter ball full game', () => {
     let rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     expect(getFinalScore(rolls)).to.deep.equal(0);
   });
 
-  it('Should get a final score from a full game with a Strike', () => {
+  it('Should get final score from full game with a Strike', () => {
     let rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 1, 0];
+
+    expect(getFinalScore(rolls)).to.deep.equal(12);
+  });
+
+  it('Should get final score from full game with multiple Strikes', () => {
+    let rolls = [10, 3, 1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+
+    expect(getFinalScore(rolls)).to.deep.equal(258);
+  });
+
+  it('Should get final score from full game with a Spare', () => {
+    let rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 1, 1, 0];
 
     expect(getFinalScore(rolls)).to.deep.equal(12);
   });
